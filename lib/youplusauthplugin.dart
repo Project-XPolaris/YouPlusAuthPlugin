bb9b5aa3fdf5256e282746d69fddbed096a80b17
allentom <qweasdzxczse2@gmail.com>
2021-08-25 22:42:08 +0800

    update

--- lib/youplusauthplugin.dart
+++ lib/youplusauthplugin.dart
@@ -5,10 +5,22 @@ import 'package:flutter/services.dart';
 
 class Youplusauthplugin {
   static const MethodChannel _channel =
-      const MethodChannel('youplusauthplugin');
+      const MethodChannel("YouPlusAuthPlugin");
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+  openYouPlus() {
+    _channel.invokeMethod("openYouPlus");
+  }
+
+  registerAuthCallback(Function(String username, String token) authCallback) {
+    _channel.setMethodCallHandler((call) async {
+      if (call.method == "authResult") {
+        authCallback(call.arguments["username"], call.arguments["token"]);
+      }
+      return;
+    });
   }
 }
